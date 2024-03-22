8363441d09daf664fad0c1a09dbb8f8549a180b1
kilarupranathi <121111893+kilarupranathi@users.noreply.github.com>
2024-03-22 14:39:31 +0530

    Update weather_modelling.py
    "Temperature modelling with hard coded variables. Keyword input, file input, single set and multiple sets"

--- weather_modelling.py
+++ weather_modelling.py
@@ -5,7 +5,7 @@ def temperature_modeling(a, b, c, time):
     # Calculate temperature based on time using the quadratic equation
     temperature = a * time**2 + b * time + c
     return temperature
-
+    
 # Hard-coded coefficients for temperature modeling
 a_hardcoded, b_hardcoded, c_hardcoded = 0.1, 2, 10
 
